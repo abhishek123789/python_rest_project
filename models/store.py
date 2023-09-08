a7d71c219266c56463439f99b4682b97e62eec08
abhishek123789 <abhishek123789@gmail.com>
2023-09-08 13:57:22 +0530

    Initial commit Working

---
+++ models/store.py
@@ -0,0 +1,12 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = "stores"
+
+    id = db.column(db.Integer, primary_key=True)
+    name = db.column(db.String(80), unique=True, nullable=False)
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
+
+
+
+
