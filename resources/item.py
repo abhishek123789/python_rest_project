1956b310aa89ffaf38fa9e4d73098cf5c4e8fbf2
abhishek123789 <abhishek123789@gmail.com>
2023-09-13 01:05:37 +0530

    Adding validation with marshmellow

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import PlainItemSchema
 
 blp = Blueprint("items", __name__, description="Operations on items")
 
@@ -26,17 +27,12 @@ class ItemList(MethodView):
     def get(self):
         return {"items": list(items.values())}
     
-    def post(self):
-        item_data = request.get_json()
-        if("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
-            abort(400, message="Bad request some required data are missing")
+    @blp.arguments(PlainItemSchema)
+    def post(self, item_data):
 
         for item in items.values():
             if(item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
                 abort(400, message="Item already exists")
-
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found")
         
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id }
